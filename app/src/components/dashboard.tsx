631812adbfc14ef034ac8ca6722bc41ad6118b7b
Kevin <kevinlu1248@gmail.com>
2021-09-24 13:03:34 -0400

    basically done dashbaord

--- app/src/components/dashboard.tsx
+++ app/src/components/dashboard.tsx
@@ -100,10 +100,16 @@ const Dashboard = () => (
       <Box sx={{ my: 4 }}/> 
       <Grid container spacing={4}>
         <Grid item xs={6}>
-            <TipCard num={2}/>
+            <img src="impact.jpg" width={"100%"} alt="about impact"></img>
         </Grid>
         <Grid item xs={6}>
-            <TipCard num={3}/>
+            <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia 
+            </Typography>
+      <Box sx={{ my: 4 }}/> 
+            <Button color="primary" variant="contained">
+                Tell me more 
+            </Button>
         </Grid>
       </Grid>
     </Box>
