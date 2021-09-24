5404c2ef1b26f6723213959ca2c9212fb8f7c7c3
Yousef Abourady <yousefabourady@gmail.com>
2021-09-23 21:39:10 -0500

    Clean up

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -14,28 +14,7 @@ import Home from "./components/home";
 import Dashboard from "./components/dashboard";
 import Leaderboard from "./components/leaderboard";
 import Profile from "./components/profile";
-
-import { initializeApp } from "firebase/app";
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-const firebaseConfig = {
-  apiKey: "AIzaSyBKS-7ecWupP7XqeSDvrC7Vn6lM-BtPMmg",
-  authDomain: "decode2021-6ea10.firebaseapp.com",
-  projectId: "decode2021-6ea10",
-  storageBucket: "decode2021-6ea10.appspot.com",
-  messagingSenderId: "43286852568",
-  appId: "1:43286852568:web:10e944cba926e1adf6f8ad",
-  measurementId: "G-ZN2DZF59KQ"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const auth = getAuth();
-// eslint-disable-next-line
-const analytics = getAnalytics(app);
+import firebaseTools from "./firebaseTools";
 
 const App = () => {
   return (
