647eb8167bae961b97efa7b1fd9f8bedb88b578c
Kevin <kevinlu1248@gmail.com>
2021-09-23 22:27:06 -0400

    finished cover screen

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -25,55 +25,55 @@ import { app, analytics } from "./firebase";
 const App = () => {
   return (
     <Router>
-        <Box sx={{ flexGrow: 1 }}>
-          <AppBar position="static">
-            <Toolbar>
-              <IconButton
-                size="large"
-                edge="start"
-                color="inherit"
-                aria-label="menu"
-                sx={{ mr: 2 }}
-                href="/"
-              >
-                <HomeIcon />
-              </IconButton>
-              <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                <Switch>
-                  <Route path="/dashboard">Dashboard</Route>
-                  <Route path="/leaderboard">Leaderboard</Route>
-                  <Route path="/profile">Profile</Route>
-                  <Route path="/">Home</Route>
-                </Switch>
-              </Typography>
-              <Button color="inherit" href="/dashboard">
-                Dashboard
-              </Button>
-              <Button color="inherit" href="/leaderboard">
-                Leaderboard
-              </Button>
-              <Button color="inherit" href="/Profile">
-                Profile
-              </Button>
-            </Toolbar>
-          </AppBar>
-        </Box>
-        <Box sx={{ m: 4 }}>
-          <Switch>
-            <Route path="/dashboard">
-              <Dashboard />
-            </Route>
-            <Route path="/leaderboard">
-              <Leaderboard />
-            </Route>
-            <Route path="/profile">
-              <Profile />
-            </Route>
-            <Route path="/">
-              <Home />
-            </Route>
-          </Switch>
-        </Box>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+              href="/"
+            >
+              <HomeIcon />
+            </IconButton>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              <Switch>
+                <Route path="/dashboard">Dashboard</Route>
+                <Route path="/leaderboard">Leaderboard</Route>
+                <Route path="/profile">Profile</Route>
+                <Route path="/">Home</Route>
+              </Switch>
+            </Typography>
+            <Button color="inherit" href="/dashboard">
+              Dashboard
+            </Button>
+            <Button color="inherit" href="/leaderboard">
+              Leaderboard
+            </Button>
+            <Button color="inherit" href="/Profile">
+              Profile
+            </Button>
+          </Toolbar>
+        </AppBar>
+      </Box>
+      <Box sx={{ m: 4 }}>
+        <Switch>
+          <Route path="/dashboard">
+            <Dashboard />
+          </Route>
+          <Route path="/leaderboard">
+            <Leaderboard />
+          </Route>
+          <Route path="/profile">
+            <Profile />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Box>
     </Router>
   );
 };
