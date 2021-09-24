3af8484063edce0d9e632a21fb210572b8ee5083
Kevin <kevinlu1248@gmail.com>
2021-09-24 11:28:55 -0400

    set up theme providers

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,13 +1,8 @@
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  // Link
-} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-import { createTheme } from '@mui/material/styles';
-import { ThemeProvider } from '@mui/styles';
+import { createTheme } from "@mui/material/styles";
+import { ThemeProvider } from "@mui/material/styles";
 
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
@@ -28,74 +23,74 @@ import { app, analytics } from "./firebase";
 const theme = createTheme({
   palette: {
     primary: {
-      light: '#757ce8',
-      main: '#3f50b5',
-      dark: '#002884',
-      contrastText: '#fff',
+      light: "#466d1d",
+      main: "#466d1d",
+      dark: "#466d1d",
+      contrastText: "#fff",
     },
     secondary: {
-      light: '#ff7961',
-      main: '#f44336',
-      dark: '#ba000d',
-      contrastText: '#000',
+      light: "#aaa",
+      main: "#888",
+      dark: "#666",
+      contrastText: "#fff",
     },
-  }
+  },
 });
 
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
-    <Router>
-      <Box sx={{ flexGrow: 1 }}>
-        <AppBar position="static">
-          <Toolbar>
-            <IconButton
-              size="large"
-              edge="start"
-              color="inherit"
-              aria-label="menu"
-              sx={{ mr: 2 }}
-              href="/"
-            >
-              <HomeIcon />
-            </IconButton>
-            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-              <Switch>
-                <Route path="/dashboard">Dashboard</Route>
-                <Route path="/leaderboard">Leaderboard</Route>
-                <Route path="/profile">Profile</Route>
-                <Route path="/">Home</Route>
-              </Switch>
-            </Typography>
-            <Button color="inherit" href="/dashboard">
-              Dashboard
-            </Button>
-            <Button color="inherit" href="/leaderboard">
-              Leaderboard
-            </Button>
-            <Button color="inherit" href="/Profile">
-              Profile
-            </Button>
-          </Toolbar>
-        </AppBar>
-      </Box>
-      <Box sx={{ m: 4 }}>
-        <Switch>
-          <Route path="/dashboard">
-            <Dashboard />
-          </Route>
-          <Route path="/leaderboard">
-            <Leaderboard />
-          </Route>
-          <Route path="/profile">
-            <Profile />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
-      </Box>
-    </Router>
+      <Router>
+        <Box sx={{ flexGrow: 1 }}>
+          <AppBar position="static">
+            <Toolbar>
+              <IconButton
+                size="large"
+                edge="start"
+                color="inherit"
+                aria-label="menu"
+                sx={{ mr: 2 }}
+                href="/"
+              >
+                <HomeIcon />
+              </IconButton>
+              <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                <Switch>
+                  <Route path="/dashboard">Dashboard</Route>
+                  <Route path="/leaderboard">Leaderboard</Route>
+                  <Route path="/profile">Profile</Route>
+                  <Route path="/">Home</Route>
+                </Switch>
+              </Typography>
+              <Button color="inherit" href="/dashboard">
+                Dashboard
+              </Button>
+              <Button color="inherit" href="/leaderboard">
+                Leaderboard
+              </Button>
+              <Button color="inherit" href="/Profile">
+                Profile
+              </Button>
+            </Toolbar>
+          </AppBar>
+        </Box>
+        <Box sx={{ m: 4 }}>
+          <Switch>
+            <Route path="/dashboard">
+              <Dashboard />
+            </Route>
+            <Route path="/leaderboard">
+              <Leaderboard />
+            </Route>
+            <Route path="/profile">
+              <Profile />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </Box>
+      </Router>
     </ThemeProvider>
   );
 };
--- app/src/components/home.tsx
+++ app/src/components/home.tsx
@@ -4,15 +4,24 @@ import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import Stack from "@mui/material/Stack";
 
-const Home = () => (
-  <Box sx={{ flexGrow: 1, textAlign: "center"}}>
-    <img src="cover.jpg" alt="the cover"></img>
-    <Box sx ={{flexGrow: 1}}>
-        <Stack spacing={2} direction="row" sx={{width: "fit-content", margin: "auto", marginTop: 2}}>
-            <Button color="primary" variant="contained">Learn More</Button>
-            <Button variant="contained">Register Your Meter</Button>
+const Home = () => {
+  // const theme =
+  return (
+    <Box sx={{ flexGrow: 1, textAlign: "center" }}>
+      <img src="cover.jpg" alt="the cover"></img>
+      <Box sx={{ flexGrow: 1 }}>
+        <Stack
+          spacing={2}
+          direction="row"
+          sx={{ width: "fit-content", margin: "auto", marginTop: 2 }}
+        >
+          <Button variant="contained">
+            Learn More
+          </Button>
+          <Button color="secondary" variant="contained">Register Your Meter</Button>
         </Stack>
+      </Box>
     </Box>
-  </Box>
-);
+  );
+};
 export default Home;
