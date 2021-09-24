8ff4adacb3c04cfed8a55a58469aaef6184a56d2
Farhaj <farhaj_khan@outlook.com>
2021-09-24 17:02:47 -0400

    test 1

--- app/src/components/leaderboard.tsx
+++ app/src/components/leaderboard.tsx
@@ -1,15 +1,12 @@
 import React from 'react';
 
-const profileCard = () => {
-    
-}
-
 const Leaderboard = () => {
     return (
-        <h1>Leaderboard</h1>
-        <img src="./LeaderboardPlaceHolder.png"></img>
-    )
+        <Router>
+            <h1>Leaderboard</h1>
+            <img src="./LeaderboardPlaceHolder.png"></img>
+        </Router>
+    );
 }
-
 
 export default Leaderboard;
